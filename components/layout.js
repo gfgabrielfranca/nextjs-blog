9fbd94be1f5977ebad25e60e5b077a21d29067b3
Gabriel França <dev.gabrielfranca@gmail.com>
2020-05-30 12:25:30 -0300

    Testing changes

--- components/layout.js
+++ components/layout.js
@@ -3,7 +3,7 @@ import utilStyles from '../styles/utils.module.css'
 import styles from './layout.module.css'
 import Link from 'next/link'
 
-const name = 'Your Name'
+const name = 'Gabriel França'
 export const siteTitle = 'Next.js Sample Website'
 
 function Layout({ children, home }) {
